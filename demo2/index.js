8dfb499c8a36e32269acbcd78e02a3984a21d27d
Michael Jolley <mike@sparcapp.io>
2019-11-19 16:19:08 -0600

    Updating code for BHM.NET

--- demo2/index.js
+++ demo2/index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const message = req.query.message || "Hello DevOpsDays Chattanooga 2019!<br/><br/>Don't forget to text <b>1-205-350-1515</b> for slides, source & more!";
+  const message =
+    req.query.message ||
+    "Hello BHM.NET Meetup!<br/><br/>Don't forget to text <b>1-205-350-1515</b> for slides, source & more!";
   res.status(200).send(message);
 });
 
